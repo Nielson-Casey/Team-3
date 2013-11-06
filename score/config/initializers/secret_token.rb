8b773072add55e72e8929a765eaef49e28d54bb3
Cameron Lewis <whitediamondmusic@gmail.com>
2013-11-05 18:24:31 -0700

    Rails Framework

---
+++ score/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Score::Application.config.secret_key_base = '4f14399022dfd1d2417742e9fb5a1f0715ad8425d87a48a5459d02de759a9d4b3c49f02b1c5e5594ac48f52d1f636f4a8f3c80815cfd857593a15ee9f27f55fd'
