ee4355a67cb3dceca0d67d4680ece81ed0812774
Cameron Lewis <whitediamondmusic@gmail.com>
2013-12-05 21:57:58 -0700

    Building User Sign Up with Devise and Omniauth
    Try navigating in your browser to:
    http://localhost:3000/users/sign_up
    
    When signing up, it throws an error.  I can’t find it, so if any of you
    guys could help me find it.  I think we just need views to navigate to
    but I’m not sure.

--- score/config/environments/development.rb
+++ score/config/environments/development.rb
@@ -26,4 +26,7 @@ Score::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Set up localhost as target
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
